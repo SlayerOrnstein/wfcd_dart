be338fd62c6262d7f1e8891b7b1b7aac3ccf5463
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2025-10-23 17:25:08 -0400

    chore: export models

--- packages/warframe_drop_data/lib/warframe_drop_data.dart
+++ packages/warframe_drop_data/lib/warframe_drop_data.dart
@@ -1,6 +1,7 @@
 /// A Very Good Project created by Very Good CLI.
 library;
 
+export 'src/models/models.dart';
 export 'src/parsers/bounty_rewards.dart';
 export 'src/parsers/parsers.dart';
 export 'src/warframe_drop_data.dart';
