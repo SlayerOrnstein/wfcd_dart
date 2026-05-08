708df92b7581286e9222bd7bcc95c7cd48ab3773
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2026-05-08 17:38:32 -0400

    chore: appease the analyzer
    Signed-off-by: SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>

--- packages/worldstate_status/routes/worldstate/index.dart
+++ packages/worldstate_status/routes/worldstate/index.dart
@@ -19,7 +19,7 @@ Future<Response> onRequest(RequestContext context) async {
   final response = await http.get(Uri.parse(_api));
   final raw = await Isolate.run(() => RawWorldstate.fromJson(response.body));
   final worldstate = await Isolate.run(() {
-    final deps = Dependency([]);
+    final deps = Dependency();
     return Worldstate.fromRaw(raw, deps);
   });
 
