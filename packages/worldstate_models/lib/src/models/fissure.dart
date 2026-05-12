d3f7ab7d4d3fa49a9857c25212d0f0c5801d275c
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2026-05-12 14:15:47 -0400

    refactor: remove unused import
    Signed-off-by: SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>

--- packages/worldstate_models/lib/src/models/fissure.dart
+++ packages/worldstate_models/lib/src/models/fissure.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:dart_mappable/dart_mappable.dart';
 import 'package:warframe_drop_data/warframe_drop_data.dart';
 import 'package:warframe_worldstate_data/warframe_worldstate_data.dart';
